9b3daa246da4595e024024198feb36397dc001f0
David Neto <jdpneto@gmail.com>
2011-08-30 14:36:07 +0100

    Preparing code for OOP

--- src/menu.java
+++ src/menu.java
@@ -9,9 +9,7 @@ import java.awt.event.ActionListener;
 public class menu extends JPanel
                         implements ActionListener {
 	
-	protected JButton random;
-	protected JButton save;
-	protected JButton restore;
+
 	
 	private int command;
 	
@@ -19,21 +17,7 @@ public class menu extends JPanel
 	{
 		command = 0;
 		
-		random = new JButton("Just Randomize");
-		save = new JButton("Randomize w/ Backup");
-		restore = new JButton("Restore Files");
-		
-		random.setHorizontalTextPosition(AbstractButton.LEADING);
-		save.setHorizontalTextPosition(AbstractButton.LEADING);
-		restore.setHorizontalTextPosition(AbstractButton.LEADING);
-		
-		random.setActionCommand("random");
-		save.setActionCommand("save");
-		restore.setActionCommand("restore");
-		
-		random.setToolTipText("Faster - Randomize the files inside the directory without the possibility of reversing the process");
-		save.setToolTipText("Randomize the files inside the directory while keeping a backup of their location");
-		restore.setToolTipText("Restore a previously randomized location to their original directories");
+
 		
 	}
 
--- src/randomizer.java
+++ src/randomizer.java
@@ -1,4 +1,6 @@
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.FileNotFoundException;
@@ -7,7 +9,10 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Random;
 
+import javax.swing.AbstractButton;
+import javax.swing.JButton;
 import javax.swing.JFileChooser;
+import javax.swing.JPanel;
 import javax.swing.UIManager;
 
 import java.io.*;
@@ -15,23 +20,47 @@ import java.util.zip.CRC32;
 import java.util.zip.Checksum;
 
 
-public class randomizer {
+public class randomizer extends JPanel implements ActionListener {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	/**
 	 * @param args
 	 */
-	
+
+	static protected JButton random_b;
+	static protected JButton restore_b;
 	//TODO
-	//Ver como funcionam as paths, tentar construir paths a partir da dir original ate ao fim e depois
-	//construir sempre dessa forma quando se vai fazer load
+	//Fazer Load, menus
+	//Criar uma class que Ž chamada do main para me ver livre dos statics todos.
 	static ArrayList<File> files = new ArrayList<File>();
 	static String dir;
 	static int random;
-	
+
 	static Hashtable<String, String> hashes = new Hashtable<String,String>();
-	
+
 	public static void main(String[] args) {
-		
+		init();
+	}
+
+	private static void init()
+	{
+		random_b = new JButton("Just Randomize");
+		restore_b = new JButton("Atempt to Restore Files");
+
+		random_b.setHorizontalTextPosition(AbstractButton.LEADING);
+		restore_b.setHorizontalTextPosition(AbstractButton.LEADING);
+
+		random_b.setActionCommand("random");
+		restore_b.setActionCommand("restore");
+
+		random_b.setToolTipText("Faster - Randomize the files inside the directory");
+		restore_b.setToolTipText("Restore a previously randomized location to its original state");
+
+		random_b.addActionListener(this);
+		restore_b.addActionListener(this);
 		//System.out.println(System.getProperty("os.name"));
 
 		//I don't want it to look ugly, so I'll go for the systems Look and Field or nothing
@@ -41,35 +70,10 @@ public class randomizer {
 			System.out.println("Error setting native LAF: " + e);
 			System.exit(1);}
 
-		//popup window to chose dir
-		dir = getDir();
-		hashes = loadFromFile();
-		//Get all mp3 files on that directory or any above it
-		getAllFiles(dir);
-		//shuffling numbers
-		int[] numbers = new int[files.size()];
-		for(int i = 0; i< files.size(); i++)
-		{
-			numbers[i]=i;
-		}
-		//avoiding possible colisions if you're randomizing a directory that was already random
-		random = getRandomFiles();
-		shuffle(numbers);
-		String[] final_names = new String[files.size()];
-		//generate random filenames
-		for(int i = 0; i< files.size(); i++)
-		{
-			final_names[i]=String.format("%010d", numbers[i])+".mp3";
-			final_names[i] = random+final_names[i];
-		}
-		//rename and / or move them to the root of that directory with their new unique names
-		File whereto = new File(dir);
-		for(int i = 0; i<files.size();i++)
-		{
-			files.get(i).renameTo(new File(whereto,final_names[i]));
-		}
-		saveToFile();
+
 	}
+
+
 	//Simple and dumb recursive method to allow
 	//That fetches all mp3 files present on
 	//the chosen directory
@@ -88,16 +92,6 @@ public class randomizer {
 				getAllFiles(fs[i].getPath());
 			else
 			{
-				//hashes.put(crc(fs[i].getPath())+""+fs[i].length(),fs[i].getAbsolutePath());
-//				System.out.println(fs[i].getAbsolutePath());
-//				try {
-//					System.out.println(fs[i].getCanonicalPath());
-//				} catch (IOException e) {
-//					// TODO Auto-generated catch block
-//					e.printStackTrace();
-//				}
-//				System.out.println(fs[i].getPath());
-				//System.out.println(getRelativePath(fs[i].getAbsolutePath()));
 				addToHash(crc(fs[i].getPath())+""+fs[i].length(),getRelativePath(fs[i].getAbsolutePath()));
 				files.add(fs[i]);
 			}
@@ -134,7 +128,7 @@ public class randomizer {
 	//just common java.swing stuff 
 	//not my strong suit, just got something together
 	//that appears to me working well enough
-	public static String getDir()
+	private static String getDir()
 	{
 		JFileChooser c;
 
@@ -152,43 +146,38 @@ public class randomizer {
 			System.exit(1);
 		return "why does he make me have a return string here if it'll exit before reaching this?";
 	}
-	
-	
+
+
 	@SuppressWarnings({ "unchecked", "unused" })
 	public static Hashtable<String,String> loadFromFile()
 	{
 		Hashtable<String,String> ret = null;
 		String path = dir+System.getProperty("file.separator")+"RESTORE_DATA_DO_NOT_DELETE.bak";
 		try {
-			
+
 			FileInputStream fs = new FileInputStream(path);
 			ObjectInputStream os = new ObjectInputStream(fs);
-			
+
 			//I added the supresswarnings, but I still want it to blow here
 			//before it can corrupt anything else, so if something's wrong
 			//it'll blow here with a ClassNotFoundException (I think)
 			//and simply return an empty hashtable
+			//check code found @StackOverflow, sorry but can't remember where
 			ret = (Hashtable<String,String>)os.readObject();
 			for (String  s : ret.keySet());
-			for (String  n : ret.values());
+					for (String  n : ret.values());
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			//e.printStackTrace();
 			ret = new Hashtable<String,String>();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			//e.printStackTrace();
 			System.out.println("Cannot access disk...");
 			System.exit(1);
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 			ret = new Hashtable<String,String>();
 		}
 		return ret;
 	}
-	
-	public static void saveToFile()
+
+	private static void saveToFile()
 	{
 		try
 		{
@@ -204,12 +193,12 @@ public class randomizer {
 			System.exit(1);
 		}
 	}
-	
-	public static String getRelativePath(String p)
+
+	private static String getRelativePath(String p)
 	{
 		return p.substring(dir.length()+1, p.length());
 	}
-	
+
 	//to have only the original dir in the recovery file
 	public static void addToHash(String hash, String location)
 	{
@@ -221,17 +210,17 @@ public class randomizer {
 	//that plus the size will give me mostly unique hashes
 	//an be fast enough to be usable
 	//some files might be too big to do a complete hash
-	public static long crc(String file) {
+	private static long crc(String file) {
 		Checksum ret = new CRC32();
 		try
 		{
 			FileInputStream fs = new FileInputStream(file);
 			BufferedInputStream bis = new BufferedInputStream(fs);
 			byte[] bytes = new byte[1024];
-			
+
 			int length = bis.read(bytes);
 			ret.update(bytes, 0, length);
-			
+
 			bis.close();
 		}
 		catch (IOException e) {
@@ -239,6 +228,43 @@ public class randomizer {
 		}
 		//System.out.println(ret.getValue());
 		return ret.getValue();
+	}
+
+	private static void randomizeDirectory()
+	{
+		//popup window to chose dir
+		dir = getDir();
+		hashes = loadFromFile();
+		//Get all mp3 files on that directory or any above it
+		getAllFiles(dir);
+		//shuffling numbers
+		int[] numbers = new int[files.size()];
+		for(int i = 0; i< files.size(); i++)
+		{
+			numbers[i]=i;
+		}
+		//avoiding possible colisions if you're randomizing a directory that was already random
+		random = getRandomFiles();
+		shuffle(numbers);
+		String[] final_names = new String[files.size()];
+		//generate random filenames
+		for(int i = 0; i< files.size(); i++)
+		{
+			final_names[i]=String.format("%010d", numbers[i])+".mp3";
+			final_names[i] = random+final_names[i];
+		}
+		//rename and / or move them to the root of that directory with their new unique names
+		File whereto = new File(dir);
+		for(int i = 0; i<files.size();i++)
+		{
+			files.get(i).renameTo(new File(whereto,final_names[i]));
+		}
+		saveToFile();		
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
